e137471a922d4878b5990d2cbe7855575a719775
Mathias Glatz <m.glatz@student.tugraz.at>
2017-05-18 16:45:33 +0200

    Added Seeding for User and Information

---
+++ database/seeds/InformationSeeder.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class InformationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('information')->insert([
+            'url' => '1',
+            'name' => '1',
+            'title' => 'Kontakt/Contact',
+            'text' => 'Fam. Gerda und Erich Jankl<br/>Innerteuchen 2<br/>A-9543 Arriach<br/><br/>Mobil: 0043 (0) 650 21 22 880<br/>www.wurzelpark.at<br/>www.steinacher-hofladen.at<br/>E-Mail: steinacher.hofladen@aon.at<br/>',
+            'url_to_image' => '',
+            'url_to_image2' => '',
+            'url_to_audio' => 'http://www.wurzelpark.at/App/Guide/Audio/HauptinfoWP.mp3',
+            'url_to_video' => '',
+            'url_to_pdf' => '',
+            'url_to_youtube' => ''
+        ]);
+
+        DB::table('information')->insert([
+            'url' => 'INFO01',
+            'name' => 'INFO01',
+            'title' => 'Der Holzfotoapparat',
+            'text' => 'Der Holzfotoapparat wurde im Jahr 2010 von Erich Jankl mit der Motorsäge geschnitzt und danach mit einer speziellen Schleifscheibe nachgearbeitet.<br/>Er wurde aus Kastanienholz gefertigt.<br/><br/>Arbeitszeit ca. 1,5 Tage.<br/><br/>Mit diesem Fotoapparat kann man aber auch richtige Fotos machen.<br/>Einfach auf den Auslöser drücken und sich 3 Meter vor dem Apparat positionieren. <br/><br/>Die Fotos sind danach in etwa 5 Tagen online auf <a href="http://www.wurzelpark.at" target="_blank">www.wurzelpark.at</a> zu sehen.<br/>',
+            'url_to_image' => 'http://www.wurzelpark.at/App/Guide/Bilder/B1.JPG',
+            'url_to_image2' => '',
+            'url_to_audio' => 'http://www.wurzelpark.at/App/Guide/Audio/DerHolzfotoapparat.mp3',
+            'url_to_video' => '',
+            'url_to_pdf' => '',
+            'url_to_youtube' => ''
+        ]);
+
+        DB::table('information')->insert([
+            'url' => 'INFO02',
+            'name' => 'INFO02',
+            'title' => 'Der Löwe',
+            'text' => 'Der Löwe wurde von Erich Jankl im Jahr 2013 gefertigt.<br/>Dieser wurde maschinell in der eigenen Werkstätte hergestellt.<br/>Er wurde dann noch manuell fertig nachbearbeitet.',
+            'url_to_image' => '',
+            'url_to_image2' => '',
+            'url_to_audio' => 'http://www.wurzelpark.at/App/Guide/Audio/Loewe.mp3',
+            'url_to_video' => '',
+            'url_to_pdf' => '',
+            'url_to_youtube' => ''
+        ]);
+    }
+}
