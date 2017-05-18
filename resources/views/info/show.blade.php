d8169dbce23aa56616fb410a9391e7c080a9ac6d
Mathias Glatz <m.glatz@student.tugraz.at>
2017-05-18 23:33:34 +0200

    Added view for station (view first question, first level)

--- resources/views/info/show.blade.php
+++ resources/views/info/show.blade.php
@@ -1,38 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-    <div classe="col-md-5">
-        <div><h1>{{ $info_item->title }}</h1></div>
+    <div class="blog-post">
+        <h1>{{ $info_item->title }}</h1>
+        <hr>
     </div>
 
-    <hr>
-
-    <div classe="col-md-5">
+    <div class="blog-post">
         <div><p>{!! $info_item->text !!}</p></div>
     </div>
 
 
-    <div classe="col-md-5">
+    <div class="blog-post">
         @if($info_item->url_to_image != null)
             <hr>
             <div><img src="{{$info_item->url_to_image}}" alt="Bild" /></div>
         @endif
     </div>
 
-    <div classe="col-md-5">
+    <div class="blog-post">
         @if($info_item->url_to_pdf != null)
             <hr>
             <div><a href="{{$info_item->url_to_pdf}}">PDF</a></div>
         @endif
     </div>
 
-    <div classe="col-md-5">
+    <div class="blog-post">
         @if($info_item->url_to_image2 != null)
             <div><img src="{{$info_item->url_to_image2}}" alt="Bild2" /></div>
         @endif
     </div>
 
-    <div classe="col-md-5">
+    <div class="blog-post">
         @if($info_item->url_to_audio != null)
             <hr>
             <audio controls>
@@ -40,7 +39,7 @@
         @endif
     </div>
 
-    <div classe="col-md-5">
+    <div class="blog-post">
         @if($info_item->url_to_video != null)
             <hr>
             <video controls>
@@ -48,7 +47,7 @@
         @endif
     </div>
 
-    <div classe="col-md-5">
+    <div class="blog-post">
         @if($info_item->url_to_youtube != null)
             <hr>
             <div><a href="{{$info_item->url_to_youtube}}">Youtube</a></div>
