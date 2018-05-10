231dd548efdb2fecbe6f7621b448e497031c39c3
Mathias Glatz <m.glatz@student.tugraz.at>
2018-05-10 17:10:33 +0200

    Updated photo album

--- resources/views/photos/show.blade.php
+++ resources/views/photos/show.blade.php
@@ -42,12 +42,20 @@
             <h2>{{ $key }}</h2>
             <div class="my-gallery container" itemscope itemtype="http://schema.org/ImageGallery">
                 @foreach ($photos_of_folder as $photo_filename=>$dimension)
-                    <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
-                        <a href="{{ asset($photo_filename) }}" itemprop="contentUrl" data-size="{{$dimension}}">
+                    <ul id="table" class="list-unstyled">
+                        <li>
+                            <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+                            <a href="{{ asset($photo_filename) }}" itemprop="contentUrl" data-size="{{$dimension}}">
                             <img src="{{ asset($photo_filename) }}" itemprop="thumbnail" alt="basename($photo_filename)" />
-                        </a>
-                        <figcaption itemprop="caption description">{{ basename($photo_filename) }}</figcaption>
-                    </figure>
+                            </a>
+                            <figcaption itemprop="caption description">{{ basename($photo_filename) }}</figcaption>
+                            </figure>
+                        </li>
+                        <li>
+                            <label>My name</label></li>
+                    </ul>
+                    <input type="checkbox"/>
+
                 @endforeach
             </div>
         @empty
